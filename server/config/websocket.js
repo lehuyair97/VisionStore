8fde6fe647560e8fb87f0b08cb511be26004899a
lehuyair <lehuyair97@gmail.com>
2024-11-02 18:43:01 +0700

    feature: Voucher & Comment - backend

---
+++ server/config/websocket.js
@@ -0,0 +1,23 @@
+// websocket.js
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  console.log("New client connected");
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+// Function to broadcast messages to all connected clients
+const broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+};
+
+module.exports = { wss, broadcast };
---
+++ server/routes/APIContainer/Voucher.js
@@ -0,0 +1,72 @@
+// controllers/voucherController.js
+const Voucher = require("./../../models/voucherModel");
+const { broadcast } = require("./../../config/websocket");
+
+exports.createVoucher = async (req, res) => {
+  try {
+    const { code, discount, expiration_date } = req.body;
+
+    const newVoucher = new Voucher({ code, discount, expiration_date });
+    await newVoucher.save();
+
+    broadcast({ type: "NEW_VOUCHER", voucher: newVoucher });
+
+    res.status(201).json(newVoucher);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.useVoucher = async (req, res) => {
+    const { code, userID } = req.body;
+    try {
+      const voucher = await Voucher.findOne({ code });
+      if (!voucher) {
+        return res.status(404).json({ message: "Voucher not found" });
+      }
+      if (voucher.status === "inactive") {
+        return res.status(400).json({ message: "Voucher is inactive" });
+      }
+      if (voucher.usedBy.some(entry => entry.userID.toString() === userID)) {
+        return res.status(400).json({ message: "You have already used this voucher" });
+      }
+      voucher.usedBy.push({ userID });
+      await voucher.save();
+      broadcast({ type: "VOUCHER_USED", voucher });
+  
+      res.status(200).json(voucher);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
+exports.getAllActiveVouchers = async (req, res) => {
+  try {
+    const vouchers = await Voucher.find({ status: "active" });
+    res.status(200).json(vouchers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getAllVouchers = async (req, res) => {
+  try {
+    const vouchers = await Voucher.find();
+    res.status(200).json(vouchers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getVoucherByCode = async (req, res) => {
+  const { code } = req.params;
+  try {
+    const voucher = await Voucher.findOne({ code });
+    if (!voucher) {
+      return res.status(404).json({ message: "Voucher not found" });
+    }
+    res.status(200).json(voucher);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
